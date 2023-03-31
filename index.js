31fa78345adf065eb0b9b490eb313bd04c2c3947
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-04-01 02:16:43 +0400

    completed fetch request

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+const quoteContent = document.querySelector('.quote__content');
+const quoteAuthor = document.querySelector('.quote__author');
 const quoteBtn = document.querySelector('.quote__btn');
-const quoteContent = document.querySelector('.quote__content');
-const quoteContent = document.querySelector('.quote__content');
 
-
+quoteContent.innerText = "Loading...";
 
 const fetchQuote = () => {
   fetch("https://api.quotable.io/random")
   .then(response => response.json())
-  .then(data =>
-
-  )
+  .then((data) => {
+    quoteContent.innerText = data.content;
+    quoteAuthor.innerText = data.author;
+  })
+  .catch((error) => {
+    quoteContent.innerText = "Error Found While Loading";
+    quoteAuthor.innerText = " ";
+  })
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  fetchQuote()
+  fetchQuote();
+})
+
+quoteBtn.addEventListener('click', () => {
+  fetchQuote();
 })
